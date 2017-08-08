98a70d498ca590b9d83f097d4c590152ed6727ae
Scott Nelson <scott@scottnelson.co>
2017-08-08 15:27:17 -0400

    Consolidate command scripts

--- scripts/command.py
+++ scripts/command.py
@@ -3,18 +3,27 @@ from serial import Serial
 import struct
 import time
 
-COMMAND = 0b0010
+CMD_READ_VOLTAGE = 0b0001
+CMD_BALANCE = 0b0010
 
 parser = argparse.ArgumentParser(description='enable 5 second balancing for a cell')
 parser.add_argument('address', type=int, help='cell monitor address')
+parser.add_argument('command', type=int, help='cell monitor command')
 parser.add_argument('--port', help='serial device name')
 
 args = parser.parse_args()
 serial = Serial(port = args.port, baudrate = 9600)
-byte = struct.pack('!B', (args.address << 4) | COMMAND)
 
-def enable_balancing():
+def send_command():
+    byte = struct.pack('!B', (args.address << 4) | args.command)
     serial.write(byte)
 
+def read_voltage():
+    values = map(ord, serial.read(2))
+    return (values[0] << 8) | values[1]
+
 if __name__ == '__main__':
-    enable_balancing()
+    send_command();
+
+    if args.command & CMD_READ_VOLTAGE:
+        print(read_voltage())
